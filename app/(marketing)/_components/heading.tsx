786018846f89a675cfb701fbce0a80bec7dd9509
Adhiraj Pawar <adhirajpawar3@gmail.com>
2024-08-09 14:33:02 +0530

    Added loading spinner to improved user experience during data fetch or loading process.

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,13 @@
 
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return(
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,9 +19,16 @@ export const Heading = () => {
                 Notion is the connected workspace where <br />
                 better, faster work happens.
             </h3>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                <Spinner size="lg"/>
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
             <Button>Enter Notion
                 <ArrowRight className="h-4 w-4 ml-2" />
             </Button>
+            )}
         </div>
     )
 }
