6202c5d5eda78352c708a5ad3248c684506defe6
Adhiraj Pawar <adhirajpawar3@gmail.com>
2024-08-11 08:50:41 +0530

    Added User Setting - Logout and sidebar functions.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,42 @@
+"use client";
+
+import  Image  from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Plus, PlusCircle } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return ( 
-        <div>
-            This is a protected page
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+         <Image 
+         src="/empty.png"
+         height="500"
+         width="500"
+         alt="Empty"
+         className="dark:hidden"
+         />
+         <Image 
+        src="/empty-dark.png"
+        height="500"
+        width="500"
+        alt="Empty"
+        className="hidden dark:block"
+        />
+        <h2 className="text-lg font-medium">
+            Welcome to {user ?.firstName}&apos;s Notion
+        </h2>
+        <Button>
+            <PlusCircle className="h-4 w-4 mr-2" />
+            Create a note
+        </Button>
         </div>
      );
 }
  
 export default DocumentsPage;
+
+
+
