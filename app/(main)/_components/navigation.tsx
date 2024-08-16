f6e96063ee21ba65ee29577b82fc41ed76458592
Adhiraj Pawar <adhirajpawar3@gmail.com>
2024-08-16 23:42:14 +0530

    Added Tashbox, Delete, Undo, ALert-popup msg.....

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronLeft, MenuIcon, PlusCircle, Settings } from "lucide-react";
+import { ChevronLeft, MenuIcon, Plus, PlusCircle, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -11,10 +11,16 @@ import { Search } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { api } from "@/convex/_generated/api";
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+ } from "@/components/ui/popover";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { DocumentList } from "./document-list";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -150,6 +156,21 @@ export const Navigation = () => {
             </div>
             <div className="mt-4">
                 <DocumentList />
+                <Item onClick={handleCreate}
+                icon={Plus}
+                label="Add a page"
+                />
+                <Popover>
+                    <PopoverTrigger className="w-full mt-4">
+                        <Item label="Trash" icon={Trash} />
+                    </PopoverTrigger>
+                    <PopoverContent
+                    className="p-0 w-72"
+                    side={isMobile ?  "bottom" : "right"}
+                    >
+                    <TrashBox />
+                    </PopoverContent>
+                </Popover>
             </div>
             <div 
                 onMouseDown={handleMouseDown}
